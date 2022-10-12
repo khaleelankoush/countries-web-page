c191d7beccf632fae5324646103c6eb727c961ab
khaleelankoush <khaleel.ankoush@gmail.com>
2022-10-13 01:30:55 +0300

    edits

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountriesModule } from './countries/countries.module';
 
 @Module({
-  imports: [CountriesModule],
+  imports: [
+    CountriesModule,
+    ThrottlerModule.forRoot({
+      limit: 3,
+      ttl: 20000,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
